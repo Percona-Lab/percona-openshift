d3368ef153d967b2fa8a30fed58f53bd2368e188
VadimTK <vadim+git@percona.com>
2018-04-30 16:44:52 -0400

    Added ReplicaSet deployment

---
+++ replicaset-image/init-datadir.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+if [ ! -z "$MYSQL_FORCE_RM_DATADIR" ]; then
+   echo "Cleaning up /var/lib/mysql"
+   rm -fr /var/lib/mysql/*
+fi
+# Skip the clone if data already exists.
+[[ -d /var/lib/mysql/mysql ]] && exit 0
+# Skip the clone on master (ordinal index 0).
+[[ `hostname` =~ ^(.*?)-([0-9]+)$ ]] || exit 1
+ordinal=${BASH_REMATCH[2]}
+rsname=${BASH_REMATCH[1]}
+servicename=$(hostname -f | cut -d"." -f2)
+echo "Detected name: $rsname $servicename"
+[[ $ordinal -eq 0 ]] && exit 0
+# Clone data from previous peer.
+ncat --recv-only ${rsname}-$(($ordinal-1)).${servicename} 3307 | xbstream -x -C /var/lib/mysql
+# Prepare the backup.
+xtrabackup --prepare --target-dir=/var/lib/mysql
+ls -lah /var/lib/mysql
