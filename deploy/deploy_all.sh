772ce41d95c93ffdfc9cf172e8c90fff0b5eb7fc
Alexander Rubin <alikrubin75@gmail.com>
2018-05-12 20:32:36 +0200

    Fixed the proxysql image to be able to configure admin user and password

---
+++ deploy/deploy_all.sh
@@ -0,0 +1,5 @@
+ocversion=`oc version|grep openshift|cut -d ' ' -f 2 2>/dev/null`
+kubeversion=`kubectl version 2>/dev/null`
+
+oc create -f secret.yaml && oc create -f mysql-configmap.yaml
+oc create -f pxc.yaml && oc create -f proxysql-pxc.yaml 
