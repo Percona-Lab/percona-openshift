c05916e3e30d55d45916fd792cc12261450dd36c
VadimTk <vadim+git@percona.com>
2018-05-22 19:50:22 -0700

    Move images

--- replicaset-image/mysqlcheck.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-# Grant privileges required:
-# GRANT PROCESS ON *.* TO 'monitor'@'localhost' IDENTIFIED BY 'monitor';
-
-mysqladmin -umonitor -p$MONITOR_PASSWORD -h127.0.0.1 ping
-exit $?
