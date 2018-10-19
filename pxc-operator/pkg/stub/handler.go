7f69a96a912a886702bf172a286d421781a8df4d
Andrew Pogrebnoy <absourd.noise@gmail.com>
2018-10-19 22:52:10 +0300

    Run replicas

--- pxc-operator/pkg/stub/handler.go
+++ pxc-operator/pkg/stub/handler.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk"
 	"github.com/sirupsen/logrus"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -26,7 +27,8 @@ func (h *Handler) Handle(ctx context.Context, event sdk.Event) error {
 
 	switch o := event.Object.(type) {
 	case *v1.PerconaXtradbCluster:
-		err := sdk.Create(newPXCPod(o))
+		// err := sdk.Create(newPXCPod(o))
+		err := sdk.Create(deploymentForPXC(o))
 		if err != nil && !errors.IsAlreadyExists(err) {
 			logrus.Errorf("failed to create busybox pod : %v", err)
 			return err
@@ -61,10 +63,53 @@ func newPXCPod(cr *v1.PerconaXtradbCluster) *corev1.Pod {
 			Containers: []corev1.Container{
 				{
 					Name:    "pxc-openshift",
-					Image:   cr.Spec.Image, //"perconalab/pxc-openshift",
+					Image:   cr.Spec.Image,
 					Command: []string{},
 				},
 			},
 		},
 	}
+}
+
+func deploymentForPXC(cr *v1.PerconaXtradbCluster) *appsv1.Deployment {
+	ls := labelsForPXC(cr.Name)
+	replicas := cr.Spec.Size
+
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name,
+			Namespace: cr.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Name:    "pxc-openshift",
+						Image:   cr.Spec.Image,
+						Command: []string{},
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 3306,
+							Name:          "mysql-port",
+						}},
+					}},
+				},
+			},
+		},
+	}
+	return dep
+}
+
+func labelsForPXC(name string) map[string]string {
+	return map[string]string{"app": "pxc-openshift", "pxc_cr": name}
 }
