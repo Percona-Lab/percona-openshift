6c61d55d03fc7f92158a7202b8ee53c7ded3d027
Mykola Marzhan <mykola.marzhan@percona.com>
2019-02-24 00:02:18 +0200

    add ProxySQL 2.0 support

---
+++ images/proxysql-image/add_pxc_nodes.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+function mysql_root_exec() {
+  local server="$1"
+  local query="$2"
+  MYSQL_PWD=${MYSQL_ROOT_PASSWORD:-password} timeout 600 mysql -h${server} -uroot -s -NB -e "${query}"
+}
+
+function wait_for_mysql() {
+    local h="$1"
+    echo "Waiting for host $h to be online..."
+    while [ "$(mysql_root_exec $h 'select 1')" != "1" ]
+    do
+        echo "MySQL is not up yet... sleeping ..."
+        sleep 1
+    done
+}
+
+function wait_for_proxy() {
+    local h=127.0.0.1
+    echo "Waiting for host $h to be online..."
+    while [ "$(MYSQL_PWD=${PROXY_ADMIN_PASSWORD:-admin} mysql -h$h -P6032 -u${PROXY_ADMIN_USER:-admin} -s -NB -e 'select 1')" != "1" ]
+    do
+        echo "ProxySQL is not up yet... sleeping ..."
+        sleep 1
+    done
+}
+
+function main() {
+    echo "Running $0"
+
+    read -ra first_host
+    if [ -z "$first_host" ]; then
+        echo "Could not find PEERS ..."
+        exit
+    fi
+
+    wait_for_mysql $first_host
+    wait_for_proxy
+
+    proxysql-admin --config-file=/etc/proxysql-admin.cnf --cluster-hostname=$first_host --disable --enable --syncusers
+    echo "All done!"
+}
+
+main
+exit 0
--- images/proxysql-image/proxysql-entry.sh
+++ images/proxysql-image/proxysql-entry.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
-set -x
 
-_PROXY_ADMIN_USER="${PROXY_ADMIN_USER:-admin}"
-_PROXY_ADMIN_PASSWORD=$(echo "${PROXY_ADMIN_PASSWORD:-admin}" | tr -cd '[:print:]')
-_PROXY_ADMIN_PORT="${PROXY_ADMIN_PORT:-6032}"
+sed -i "s/admin:admin/admin:$PROXY_ADMIN_PASSWORD/g"                               /etc/proxysql/proxysql.cnf
+sed -i "s/cluster_username=\"admin\"/cluster_username=\"admin\"/g"                 /etc/proxysql/proxysql.cnf
+sed -i "s/cluster_password=\"admin\"/cluster_password=\"$PROXY_ADMIN_PASSWORD\"/g" /etc/proxysql/proxysql.cnf
+sed "s/PROXYSQL_USERNAME='admin'/PROXYSQL_USERNAME='admin'/g"                      /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed "s/PROXYSQL_PASSWORD='admin'/PROXYSQL_PASSWORD='$PROXY_ADMIN_PASSWORD'/g"      /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed "s/CLUSTER_USERNAME='admin'/CLUSTER_USERNAME='root'/g"                         /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed "s/CLUSTER_PASSWORD='admin'/CLUSTER_PASSWORD='$MYSQL_ROOT_PASSWORD'/g"         /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed "s/MONITOR_USERNAME='monitor'/MONITOR_USERNAME='monitor'/g"                    /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed "s/MONITOR_PASSWORD='monitor'/MONITOR_PASSWORD='$MONITOR_PASSWORD'/g"          /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
 
-sed -i "s/admin:admin/$_PROXY_ADMIN_USER:$_PROXY_ADMIN_PASSWORD/g" /etc/proxysql/proxysql.cnf &>>/tmp/sed
-sed -i "s/0.0.0.0:6032/0.0.0.0:$_PROXY_ADMIN_PORT/g" /etc/proxysql/proxysql.cnf &>>/tmp/sed
-sed "s/PROXYSQL_USERNAME='admin'/PROXYSQL_USERNAME='$_PROXY_ADMIN_USER'/g" /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
-sed "s/PROXYSQL_PASSWORD='admin'/PROXYSQL_PASSWORD='$_PROXY_ADMIN_PASSWORD'/g" /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf 
-sed "s/CLUSTER_USERNAME='admin'/CLUSTER_USERNAME='root'/g" /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
-sed "s/CLUSTER_PASSWORD='admin'/CLUSTER_PASSWORD='$MYSQL_ROOT_PASSWORD'/g" /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
-
-#cp /etc/proxysql.cnf /tmp
-
-function add_peers {
- /usr/bin/peer-list -on-start="/usr/bin/add_cluster_nodes.sh" -service="$PXCSERVICE"
-# TODO: for replicaset we need to use "-on-change" 
-}
-
-add_peers &
-
-exec /usr/bin/proxysql -f -c /etc/proxysql/proxysql.cnf
+exec "$@"
