afac9fa07a893ae75caeb668ecf2338a72abcc93
Mykola Marzhan <mykola.marzhan@percona.com>
2019-03-06 00:06:53 +0200

    add ProxySQL 2.0 support

--- images/proxysql-image/proxysql-entry.sh
+++ images/proxysql-image/proxysql-entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i "s/admin:admin/admin:$PROXY_ADMIN_PASSWORD/g"                               /etc/proxysql/proxysql.cnf
-sed -i "s/cluster_username=\"admin\"/cluster_username=\"admin\"/g"                 /etc/proxysql/proxysql.cnf
-sed -i "s/cluster_password=\"admin\"/cluster_password=\"$PROXY_ADMIN_PASSWORD\"/g" /etc/proxysql/proxysql.cnf
-sed "s/PROXYSQL_USERNAME='admin'/PROXYSQL_USERNAME='admin'/g"                      /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
+sed -i "s/\"admin:admin\"/\"${PROXY_ADMIN_USER:-admin}:$PROXY_ADMIN_PASSWORD\"/g"       /etc/proxysql/proxysql.cnf
+sed -i "s/cluster_username=\"admin\"/cluster_username=\"${PROXY_ADMIN_USER:-admin}\"/g" /etc/proxysql/proxysql.cnf
+sed -i "s/cluster_password=\"admin\"/cluster_password=\"$PROXY_ADMIN_PASSWORD\"/g"      /etc/proxysql/proxysql.cnf
+sed "s/PROXYSQL_USERNAME='admin'/PROXYSQL_USERNAME='${PROXY_ADMIN_USER:-admin}'/g" /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
 sed "s/PROXYSQL_PASSWORD='admin'/PROXYSQL_PASSWORD='$PROXY_ADMIN_PASSWORD'/g"      /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
 sed "s/CLUSTER_USERNAME='admin'/CLUSTER_USERNAME='root'/g"                         /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
 sed "s/CLUSTER_PASSWORD='admin'/CLUSTER_PASSWORD='$MYSQL_ROOT_PASSWORD'/g"         /etc/proxysql-admin.cnf 1<> /etc/proxysql-admin.cnf
