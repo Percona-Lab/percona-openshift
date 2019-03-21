c56e0c09203be907d21e26490f29e9742d85d648
Mykola Marzhan <mykola.marzhan@percona.com>
2019-03-21 14:10:52 +0200

    add minio client

--- images/backup-image/backup.sh
+++ images/backup-image/backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# expect NODE_NAME to take backup
 
 set -o errexit
 set -o xtrace
@@ -15,18 +14,26 @@ function backup_volume() {
     echo "Backup finished"
 
     stat xtrabackup.stream
+    if [ $(stat -c%s xtrabackup.stream) = 0 ]; then
+        exit 1
+    fi
     md5sum xtrabackup.stream | tee md5sum.txt
 }
 
 function backup_s3() {
-    ARGS=()
-    if [ -n "$AWS_ENDPOINT_URL" ]; then
-        ARRAY+=('--endpoint', "$AWS_ENDPOINT_URL")
-    fi
-    echo "Backup to $AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH started"
+    AWS_S3_BUCKET_PATH=${AWS_S3_BUCKET_PATH:-$NODE_NAME-$(date +%F-%H-%M)-xtrabackup.stream}
+
+    echo "Backup to s3://$AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH started"
+    mc -C /tmp/mc config host add dest "${AWS_ENDPOINT_URL:-https://s3.amazonaws.com}" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
     ncat --recv-only "$NODE_NAME" 3307 \
-        | gof3r put --bucket "$AWS_S3_BUCKET" ${ARRAY[@]} --key $AWS_S3_BUCKET_PATH/$NODE_NAME-$(date +%F-%H-%M)-xtrabackup.stream
+        | mc -C /tmp/mc pipe "dest/$AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH"
     echo "Backup finished"
+
+    mc -C /tmp/mc stat "dest/$AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH"
+    s3_size=$(mc -C /tmp/mc stat "dest/$AWS_S3_BUCKET/$AWS_S3_BUCKET_PATH" | grep "^Size" | awk '{print$3}')
+    if [ $s3_size = "0B" ]; then
+        exit 1
+    fi
 }
 
 if [ -n "$AWS_S3_BUCKET" ]; then
