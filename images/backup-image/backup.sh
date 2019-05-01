4709c73e535cc31d7e54dd8f75664045d6a66e83
Mykola Marzhan <mykola.marzhan@percona.com>
2019-05-01 22:58:01 +0300

    CLOUD-203 use different certificates for PXC and ProxySQL traffic

--- images/backup-image/backup.sh
+++ images/backup-image/backup.sh
@@ -16,16 +16,26 @@ function get_backup_source() {
 }
 
 function check_ssl() {
-    local CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+    CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
     if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then
         CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
     fi
-    if [ -f /etc/mysql/ssl/ca.crt ]; then
-        CA=/etc/mysql/ssl/ca.crt
+    SSL_DIR=${SSL_DIR:-/etc/mysql/ssl}
+    if [ -f ${SSL_DIR}/ca.crt ]; then
+        CA=${SSL_DIR}/ca.crt
+    fi
+    SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/mysql/ssl-internal}
+    if [ -f ${SSL_INTERNAL_DIR}/ca.crt ]; then
+        CA=${SSL_INTERNAL_DIR}/ca.crt
     fi
 
-    local KEY=/etc/mysql/ssl/tls.key
-    local CERT=/etc/mysql/ssl/tls.crt
+    KEY=${SSL_DIR}/tls.key
+    CERT=${SSL_DIR}/tls.crt
+    if [ -f ${SSL_INTERNAL_DIR}/tls.key -a -f ${SSL_INTERNAL_DIR}/tls.crt ]; then
+        KEY=${SSL_INTERNAL_DIR}/tls.key
+        CERT=${SSL_INTERNAL_DIR}/tls.crt
+    fi
+
     if [ -f $CA -a -f $KEY -a -f $CERT ]; then
         GARBD_OPTS="-o socket.ssl_ca=$CA;socket.ssl_cert=$CERT;socket.ssl_key=$KEY;socket.ssl_cipher="
         SOCAT_OPTS="openssl-listen:4444,reuseaddr,cert=$CERT,key=$KEY,cafile=$CA,verify=1,retry=30"
