d218106616b884974838e6c0c1863560fa366990
Mykola Marzhan <mykola.marzhan@percona.com>
2019-05-07 19:05:50 +0300

    add SSL for PVC recovery

--- images/backup-image/recovery-pvc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-set -o xtrace
-
-ping -c1 $RESTORE_SRC_SERVICE || :
-rm -rf /datadir/*
-ncat $RESTORE_SRC_SERVICE 3307 | xbstream -x -C /datadir
-xtrabackup --prepare --target-dir=/datadir ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY}
