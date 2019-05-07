89295076bfea4a3b34d83b924a3033be24082896
Mykola Marzhan <mykola.marzhan@percona.com>
2019-05-07 19:48:11 +0300

    use xbcloud instead of minio client

--- images/backup-image/recovery-s3.sh
+++ images/backup-image/recovery-s3.sh
@@ -3,8 +3,10 @@
 set -o errexit
 set -o xtrace
 
-mc -C /tmp/mc config host add dest "${AWS_ENDPOINT_URL:-https://s3.amazonaws.com}" "$AWS_ACCESS_KEY_ID" "$AWS_SECRET_ACCESS_KEY"
-mc -C /tmp/mc ls dest/${S3_BUCKET_URL}
+mc -C /tmp/mc config host add dest "${ENDPOINT:-https://s3.amazonaws.com}" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
+mc -C /tmp/mc ls "dest/${S3_BUCKET_URL}"
+
 rm -rf /datadir/*
-mc -C /tmp/mc cat dest/${S3_BUCKET_URL} | xbstream -x -C /datadir
+xbcloud get "s3://${S3_BUCKET_URL}" --parallel=10 \
+    | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
 xtrabackup --prepare --target-dir=/datadir ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY}
